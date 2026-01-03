e00ad4792dd77e4f27ebf6dfefa06b35e576c773
Krone107 <kwn.works@gmail.com>
2026-01-03 22:33:39 +0800

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -2,3 +2,9 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+<script>
+  if (window.location.href.endsWith(".html")) {
+    window.location.href = window.location.href.replace(".html", "");
+  }
+</script>
